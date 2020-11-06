869cfdfec681631ac5f0691e280c4708d50840d0
jglanglois <langlois.jeangil@gmail.com>
2020-11-06 02:59:43 +0100

    Create HUD widget
    Create custom HUDWidget class.
    Add a FText variable and get/set functions. Make get BlueprintPure.

---
+++ Source/EscapeRoom/Public/HUDWidget.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API UHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	FText InfoText = FText();
+
+public:
+
+	UFUNCTION(BlueprintPure)
+	FText GetInfoText() const;
+	
+	void SetInfoText(FText NewInfoText);
+	
+};
