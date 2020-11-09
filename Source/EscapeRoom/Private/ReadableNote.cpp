5c599c4015f28b01b7043edfbbbdca2e20af7590
jglanglois <langlois.jeangil@gmail.com>
2020-11-09 07:30:10 +0100

    Add ReadableNote class
    New class has a mash and a UserWidget, and implements InteractInterface.
    When interacted with, add the widget to the custom PlayerController and lock player in place.
    Add mesh, material, widgetBP and BP.

---
+++ Source/EscapeRoom/Private/ReadableNote.cpp
@@ -0,0 +1,63 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ReadableNote.h"
+#include "Blueprint/UserWidget.h"
+#include "EscapeRoomPlayerController.h"
+#include "PlayerCharacter.h"
+
+// Sets default values
+AReadableNote::AReadableNote()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
+	RootComponent = BaseMesh;
+}
+
+// Called when the game starts or when spawned
+void AReadableNote::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PlayerControllerRef = GetWorld()->GetFirstPlayerController();
+	if(PlayerControllerRef != nullptr){
+		ER_PlayerControllerRef = Cast<AEscapeRoomPlayerController>(PlayerControllerRef);
+
+		if(NoteWidgetClass != nullptr)
+		{
+			NoteWidget = CreateWidget(PlayerControllerRef, NoteWidgetClass);
+		}
+	}
+}
+
+// Called every frame
+void AReadableNote::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FText AReadableNote::GetInfoText() 
+{
+	return FText::FromString("Read");
+}
+
+void AReadableNote::Interact(AActor* Caller) 
+{
+	// Display Widget
+	if(ER_PlayerControllerRef != nullptr)
+	{
+		ER_PlayerControllerRef->SetFocusWidget(NoteWidget);
+	}
+	// Lock Player in place
+	if(Caller != nullptr)
+	{
+		APlayerCharacter* ER_PlayerCharacter = Cast<APlayerCharacter>(Caller);
+		if(ER_PlayerCharacter != nullptr)
+		{
+			ER_PlayerCharacter->SetPlayerCanMove(false);
+		}
+	}
+}
---
+++ Source/EscapeRoom/Public/ReadableNote.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractInterface.h"
+#include "ReadableNote.generated.h"
+
+class UUserWidget;
+class AEscapeRoomPlayerController;
+
+UCLASS()
+class ESCAPEROOM_API AReadableNote : public AActor, public IInteractInterface
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* BaseMesh;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UUserWidget> NoteWidgetClass;
+
+	UPROPERTY()
+	UUserWidget* NoteWidget;
+
+	UPROPERTY()
+	AEscapeRoomPlayerController* ER_PlayerControllerRef = nullptr;
+
+public:	
+	// Sets default values for this actor's properties
+	AReadableNote();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual FText GetInfoText() override;
+	virtual void Interact(AActor* Caller) override;
+};
