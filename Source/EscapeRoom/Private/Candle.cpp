f56c524145ab10508cb697b6bed3f2113938aa8f
jglanglois <langlois.jeangil@gmail.com>
2020-11-12 10:54:45 +0100

    Cleanup Candle and add doc

--- Source/EscapeRoom/Private/Candle.cpp
+++ Source/EscapeRoom/Private/Candle.cpp
@@ -4,11 +4,10 @@
 #include "Candle.h"
 #include "Components/PointLightComponent.h"
 
-// Sets default values
 ACandle::ACandle()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
 	RootComponent = BaseMesh;
@@ -17,41 +16,13 @@ ACandle::ACandle()
 
 }
 
-// Called when the game starts or when spawned
-void ACandle::BeginPlay()
-{
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void ACandle::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
 FText ACandle::GetInfoText() 
 {
-	if(FlameLight->IsVisible())
-	{
-		return TurnOffText;
-	}
-	else
-	{
-		return TurnOnText;
-	}
+	return (FlameLight->IsVisible()) ? TurnOffText : TurnOnText;
 }
 
 void ACandle::Interact(AActor* Caller) 
 {
-	if(FlameLight->IsVisible())
-	{
-		FlameLight->SetVisibility(false);
-	}
-	else
-	{
-		FlameLight->SetVisibility(true);
-	}
+	(FlameLight->IsVisible()) ? FlameLight->SetVisibility(false) :FlameLight->SetVisibility(true);
 }
 
--- Source/EscapeRoom/Public/Candle.h
+++ Source/EscapeRoom/Public/Candle.h
@@ -9,34 +9,34 @@
 
 class UPointLightComponent;
 
+/**
+ *	Actor representing a light the player can turn on and off. 
+ */
 UCLASS()
 class ESCAPEROOM_API ACandle : public AActor, public IInteractInterface
 {
 	GENERATED_BODY()
 
+	/** Actor mesh to be set in the editor */
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* BaseMesh;
 
+	/** PointLight that can be turnd on and off */
 	UPROPERTY(VisibleAnywhere)
 	UPointLightComponent* FlameLight;
 
+	/** Text to display if the player can reach the actor and the light if turned off */
 	UPROPERTY(EditAnywhere)
 	FText TurnOnText;
+
+	/** Text to display if the player can reach the actor and the light if turned on */
 	UPROPERTY(EditAnywhere)
 	FText TurnOffText;
 	
 public:	
-	// Sets default values for this actor's properties
 	ACandle();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
+	// Interact Interface overrides
 	virtual FText GetInfoText() override;
 	virtual void Interact(AActor* Caller) override;
 };
